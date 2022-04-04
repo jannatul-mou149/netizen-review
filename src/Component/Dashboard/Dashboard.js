52eb03c6b0077f5e44584e637bff7caeae2fc3f1
jannatul-mou149 <jannatul15-12149@gmail.com>
2022-04-04 19:57:17 +0600

    dashboard done

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -1,9 +1,77 @@
 import React from 'react';
+import { Line, LineChart, CartesianGrid, XAxis, YAxis, BarChart, Tooltip, Legend, Bar } from 'recharts';
 
 const Dashboard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 9241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 12423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 17226,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 20529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 29601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 36700,
+            "revenue": 61000
+        }
+    ]
     return (
-        <div>
-            <h1>This is dashboard</h1>
+        <div className='grid items-center w-full grid-cols-1 gap-10 mx-auto md:w-4/5 lg:grid-cols-2 xl:gap-32 mt-8 h-[75vh]'>
+            <LineChart width={500} height={300} data={data}>
+                <CartesianGrid stroke="#ccc" />
+                <Line dataKey={'investment'}></Line>
+                <Line dataKey={'revenue'}></Line>
+                <Line dataKey={'sell'}></Line>
+                <XAxis dataKey={'month'}></XAxis>
+                <Tooltip />
+                <Legend />
+                <YAxis></YAxis>
+            </LineChart>
+            <BarChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey={'month'} />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="investment" fill="#8884d8" />
+                <Bar dataKey="revenue" fill="#82ca9d" />
+                <Bar dataKey="sell" fill="#5713bd" />
+            </BarChart>
         </div>
     );
 };
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
         <div className='md:px-16 py-8'>
             <div className='grid items-center w-full grid-cols-1 gap-10 mx-auto md:w-4/5 lg:grid-cols-2 xl:gap-32'>
                 <div>
-                    <h1 className='mb-4 text-2xl font-extrabold leading-tight tracking-tight text-left text-gray-900 md:text-4xl'>
+                    <h1 className='mb-4 text-2xl font-extrabold leading-tight tracking-tight text-left text-blue-900 md:text-4xl'>
                         Looking For PS5 Review Before Buying?
                     </h1>
                     <p className='mb-5 text-base text-left text-gray-800 md:text-xl'>
@@ -26,7 +26,7 @@ const Home = () => {
                 <img src="https://games4u.pk/wp-content/uploads/2021/06/Sony-PlayStation-5-Disc-Edition.png" alt="" />
             </div>
             <div>
-                <h1 className='mt-40 mb-24 text-2xl font-extrabold leading-tight tracking-tight text-gray-900 md:text-4xl text-center'>Customer Review</h1>
+                <h1 className='mt-40 mb-24 text-2xl font-extrabold leading-tight tracking-tight text-blue-900 md:text-4xl text-center'>Customer Review</h1>
             </div>
             <div className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
                 <div className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 justify-items-center'>
